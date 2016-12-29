db0c6ee3ca20a2155b35cd6981924daad9156826
Hero <lhard@qq.com>
2016-12-29 18:43:05 +0800

    main.js components question

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h3>值日预览</h3>
+    <h3>值日预览{{msg}}</h3>
     <div class="panel panel-default" v-for="item in ['一','二','三','四','五','六']">
       <div class="panel-heading">周{{item}}
         <div class="dropdown">
@@ -11,16 +11,30 @@
         </div>
       </div>
       <div class="panel-body">
-        <div v-for="item in [1,2,3,4,5,6]">
+        <div class="label-item" v-for="item in [1,2,3,4,5,6]">
           <span class="label label-primary">刘天青</span>
-          <i class="glyphicon glyphicon-remove"></i>
+          <i class="glyphicon glyphicon-remove remove" ></i>
         </div>
       </div>
     </div>
   </div>
 </template>
 
+<script>
+  export default{
+    data:function(){
+      return {
+        msg:'abc'
+      }
+    },
+    methods:{
+
+    }
+  }
+</script>
+
 <style>
+/*bootstrap 部分*/
 .panel-heading{
   padding: 5px 15px;
 }
@@ -29,26 +43,36 @@
   top: 1px;
   left: 95px;
 }
-
-#app .panel{
+.panel{
   width: 245px;
   height: 150px;
   float: left;
   margin-right: 10px;
 }
-#app .panel-body>div{
+/*自定义部分*/
+.label-item{
   width: 70px;
   height: 30px;
   float: left;
   cursor: default;
 }
-#app .panel-body>span{
+.label-item：hover{
+  color: red;
 }
-#app .panel-body>div>i{
+.label-item>i{
   display: none;
   top: -5px;
   right: 10px;
   color: #D84A4A;
   cursor: pointer;
 }
+
+/*.label-item{
+  >.remove{
+    display:none;
+  }
+  &:hover .remove{
+    display:block;
+  }
+}*/
 </style>
--- src/main.js
+++ src/main.js
@@ -11,6 +11,8 @@ const router = new VueRouter(routerConfig)
 new Vue({
   el: '#app',
   router,
+  //这个app标签是下行的components定义的组件。此处的用法相当于在，index.html页面内的#app元素下面定义一下<app/>标签
   template: '<App/>',
+  //表示外界使用模板引用的模板名，此处是简写，全写是：'App':App，分号后的App指引入的App.vue文件。
   components: { App }
 })
