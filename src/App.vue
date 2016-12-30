46f9ea9d8c0b92ab8cb19c4eb51447d99242e83a
Hero <lhard@qq.com>
2016-12-30 15:00:30 +0800

    dynamic navigation

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,46 @@
 <template>
-  <div>
-    <transition enter-active-class="animated fadeInRightBig" leave-active-class="hide">
-      <router-view></router-view>
-    </transition>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-3 col-md-2 sidebar">
+        <ul class="nav nav-sidebar">
+          <li v-for="item in list">
+            <router-link :to="item.link">{{item.name}}</router-link>
+          </li>
+        </ul>
+      </div>
+      <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+        <transition enter-active-class="animated fadeInRightBig" leave-active-class="hide">
+          <router-view></router-view>
+        </transition>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 import '../node_modules/animate.css/animate.css'
 export default {
-  name: 'app'
+  //将li变为循环添加
+  data() {
+    return {
+      list: [{
+        'id': 1,
+        'name': '值日管理',
+        'link': 'overview'
+      }, {
+        'id': 2,
+        'name': '成员管理',
+        'link': 'addmember'
+      }, {
+        'id': 3,
+        'name': '系统设置',
+        'link': 'systemset'
+      }]
+    }
+  }
 }
 </script>
 <style>
-.hide {
-  display: none;
+.router-link-active {
+  background-color: red;
 }
 </style>
