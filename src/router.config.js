160faef8910937e1c95e9c4dde13f466d76fd011
Hero <lhard@qq.com>
2016-12-30 11:23:52 +0800

    add annotation,say:router.config.js is only a configuration file

--- src/router.config.js
+++ src/router.config.js
@@ -2,7 +2,7 @@ import Overview from './components/Overview.vue'
 import AddMember from './components/AddMember.vue'
 //定义懒加载方式
 const Hello = resolve => require(['./components/Hello.vue'], resolve)
-
+  //此文件仅仅是路由的配置文件
 export default {
   routes: [
     { path: '/overview', component: Overview },
